ce0fa4cfa145f93146195aebe5c5a92889146162
SagarVartak <sagarvartak25@gmail.com>
2022-12-30 17:55:40 +0530

    added changes log0
    styles
    
    page
    
    static

--- src/App.js
+++ src/App.js
@@ -2,51 +2,63 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
 import Youtube from "./pages/youtube";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faYoutube} from '@fortawesome/free-brands-svg-icons'
+import {faMagnifyingGlass, faTrash} from '@fortawesome/free-solid-svg-icons'
+import newLogo from "./static/yt1.png"
+
 function App() {
   const [url, setUrl] = useState("");
   const [vid, setVid] = useState([]);
 
   const handleChange = (event) => {
-    console.log("value", event.target.value);
     setUrl(event.target.value);
-    console.log("sagar", url);
   };
 
   const handleClick = async (e) => {
     e.preventDefault();
-      await fetch(process.env.REACT_APP_URL + url)
-        .then((res) => res.json())
-        .then((data) => {
-          setVid(data);
-          console.log(data);
-        });
-     console.log('video',vid);  
-  
+    await fetch(process.env.REACT_APP_URL + url)
+      .then((res) => res.json())
+      .then((data) => {
+        setVid(data);
+      });
   };
 
-  const handleClear = (e) =>{
+  const handleClear = (e) => {
     e.preventDefault();
     setUrl("");
     setVid([]);
-    document.getElementById('url').value="";
-  }
+    document.getElementById("url").value = "";
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h2 className="hero">YOUTUBE</h2>
-        <h6 className="heroLabel">Downloader</h6>
-        <h3 className="clear" onClick={handleClear}>Trash</h3>
-        <form className="formBox">
-          <input className="ytUrl" id="url" onChange={handleChange}></input>
-          <button className="search" onClick={handleClick} value="submit">
-            search
-          </button>
-        </form>
-        <div>
-        {(vid.length == 0)?(<></>):(<Youtube video={vid}/>)}
+      <FontAwesomeIcon icon={faYoutube} size="4x" className="ytIcon" />
+        <div className="upDown">
+        <img src={newLogo} alt="logo" height="90vh"/>
         </div>
+        
       </header>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="text"
+            className="searchTerm"
+            placeholder="Enter url here ..."
+            id="url"
+            onChange={handleChange}
+          />
+          <button type="submit" className="searchButton">
+            <div  onClick={handleClick} className='mag'><FontAwesomeIcon icon={faMagnifyingGlass}/></div>
+          <div onClick={handleClear} ><FontAwesomeIcon icon={faTrash}/></div>
+          </button>
+          
+        </div>
+        
+        
+      </div>
+      <div>{vid.length === 0 ? <></> : <Youtube video={vid} />}</div>
     </div>
   );
 }
--- src/pages/youtube.jsx
+++ src/pages/youtube.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "../styles/video.module.css"
+import styles from "../styles/video.module.css"
 export default function youtube(video) {
   console.log("props", video);
   return (
     <>
-      <div>youtube</div>
-      <div>
+      {/* <div>youtube</div> */}
+      <div className={styles.thumb}>
         <img src={video.video.info.thumbnail} alt='thumbnail' height='100px'/>
         <p>{video.video.info.title}</p>
       </div>
@@ -13,15 +13,15 @@ export default function youtube(video) {
         {video.video.sources.map((source,index) => {
             return(
           <ul key={index}>
-            <li>{source.resolution}</li>
-            <li>{source.size}</li>
+            <li>Quality: {source.resolution}</li>
+            <li>Size: {source.size}</li>
             <li>
               <a
                 href={source.url}
                 download={video.video.info.title}
                 
               >
-                <button className="btn-Download">Download</button>
+                <button className="btn">Download</button>
               </a>
             </li>
           </ul>
