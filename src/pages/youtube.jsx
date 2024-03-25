c16f059652330e81e36842efe1a5747c824da4cc
SagarVartak <sagarvartak25@gmail.com>
2024-03-25 06:19:08 -0300

    api changes, style updates

--- src/pages/youtube.jsx
+++ src/pages/youtube.jsx
@@ -4,15 +4,6 @@ import styles from "../styles/video.module.css"
 
 export default function youtube(video) {
 
-  const handleClick = (url,fileName) =>{
-    const aTag = document.createElement("a");
-    aTag.href =url;
-    aTag.setAttribute("download",fileName);
-    document.body.appendChild(aTag);
-    aTag.click();
-    aTag.remove();
-  }
-
   console.log("props", video);
   return (
     <>
